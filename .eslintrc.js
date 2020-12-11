a88c0cadec0d84cf79bb7d2aa77f729b5dd92921
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-12-11 17:40:31 +0530

    Use eslint

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+/* eslint-disable quote-props */
+const defaultTSOptions = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    'comma-spacing': 'off',
+    '@typescript-eslint/comma-spacing': 'error',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single', { avoidEscape: true }],
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-for-in-array': 'error',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }]
+  }
+}
+
+module.exports = {
+  extends: ['eslint:recommended'],
+  env: {
+    es2020: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'linebreak-style': ['error', 'windows'],
+    'eqeqeq': ['error', 'smart'],
+    'arrow-parens': ['error', 'always'],
+    'no-var': 'error',
+    'no-unneeded-ternary': 'error',
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+    'quote-props': ['error', 'as-needed']
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      ...defaultTSOptions
+    },
+    {
+      files: ['*.test.ts'],
+      ...defaultTSOptions,
+      parserOptions: {
+        ...defaultTSOptions.parserOptions,
+        project: './tests/tsconfig.json'
+      },
+      env: {
+        es2020: true,
+        node: true,
+        jest: true
+      }
+    }
+  ]
+};
