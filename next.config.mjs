b8b8327b64c5b063f71ac2b28571dcc901f2926e
sarsamurmu <sarsamurmu05@gmail.com>
2024-05-11 21:01:50 +0530

    Fix output

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,7 @@ import path from 'path'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
-  sassOptions: {
-    // includePaths: [path.join(__dirname, 'styles')],
-  },
-  // transpilePackages: ['@react-syntax-highlighter/dist/esm/languages/prism']
+  output: 'export'
 }
 
 const withMDX = createMDX({
