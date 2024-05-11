b57d902037de45ceb7151c6ee58fb1cb0e6f2974
sarsamurmu <sarsamurmu05@gmail.com>
2024-05-11 21:54:19 +0530

    Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
-import createMDX from '@next/mdx'
-import remarkFM from 'remark-frontmatter'
-import path from 'path'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
   output: 'export'
 }
 
-const withMDX = createMDX({
-  extension: /\.(md|mdx)$/,
-  options: {
-    remarkPlugins: [remarkFM],
-    rehypePlugins: [],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,7 +16,7 @@ export default function Document() {
           `
           }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Lexend+Deca:wght@100..900&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
       </Head>
       <body>
