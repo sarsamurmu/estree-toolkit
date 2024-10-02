67216b8bf9eef18bfb0cbad62ec4663bb1402285
sarsamurmu <sarsamurmu05@gmail.com>
2024-10-02 18:19:50 +0530

    Add clouflare analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
 import 'simplebar-react/dist/simplebar.min.css'
 import '@/styles/global.scss'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps, ...rest }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        defer
+        data-cf-beacon='{"token": "d10df4b8bf7e4025adf218621ad18736"}'
+        src='https://static.cloudflareinsights.com/beacon.min.js' />
+    </>
+  )
 }
