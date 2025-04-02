e6223e74ceaf2323f17b5e196f5dfc9650059103
sarsamurmu <sarsamurmu05@gmail.com>
2025-04-02 20:48:54 +0530

    Use DM Sans as the default font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,7 +17,7 @@ export default function Document() {
           }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Lexend+Deca:wght@100..900&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Lexend+Deca:wght@100..900&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
